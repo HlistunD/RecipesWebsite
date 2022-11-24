fefd2ff57de787d6da8ce5fb309b9fe13a2e0925
Dasha Khlistun <dashakhlistun@MacBook-Air-Dasha.local>
2022-11-23 19:08:59 -0500

    message

--- src/App.js
+++ src/App.js
@@ -3,67 +3,54 @@ import "./App.css";
 import video from "./Cook.mp4";
 import arrow from "./arrow.png";
 import Recipies from "./Recipies";
-
 export default function App() {
-
-  const MY_ID = "7dfe2c77";
-  const MY_KEY = "ece7be3b8975991aa4a297268d004c6e";
-
-  const [mySearch, setMySearch] = useState ("");
-  const [showRecipes, setShowResipes] = useState([]);
-  const [wordSub, setWordSub] = useState ("avocado");
-
-  useEffect (() => {
-    async function catchFetch() {
-    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSub}&app_id=${MY_ID}&app_key=${MY_KEY}`);
-    const data = await response.json();
-    setShowResipes(data.hits)
-  }
-    catchFetch()
-  }, [wordSub])
-
-  const myRecipeSearch = (e) => {
-  setMySearch(e.target.value)
-  }
-
+const MY_ID = "7dfe2c77";
+const MY_KEY = "ece7be3b8975991aa4a297268d004c6e";
+const [mySearch, setMySearch] = useState ("");
+const [showRecipes, setShowResipes] = useState([]);
+const [wordSub, setWordSub] = useState ("avocado");
+useEffect (() => {
+async function catchFetch() {
+const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSub}&app_id=${MY_ID}&app_key=${MY_KEY}`);
+const data = await response.json();
+setShowResipes(data.hits)
+}
+catchFetch()
+}, [wordSub])
+const myRecipeSearch = (e) => {
+setMySearch(e.target.value)
+}
 const finalSearch = (e) => {
 e.preventDefault();
 setWordSub(mySearch);
 }
-
-  return(
-  <div className="App">
-    <div className="container">
+return(
+<div className="App">
+   <div className="container">
       <video autoPlay muted loop>
-        <source src={video} type="video/mp4"/>
+         <source src={video} type="video/mp4"/>
       </video>
       <h1>Let's find your recipe</h1>
-  </div>
-
-    <div className="container">
+   </div>
+   <div className="container">
       <form onSubmit={finalSearch}> 
-        <input placeholder="Pet here your prodact..." className="search" onChange={myRecipeSearch} value={mySearch}/>
+         <input placeholder="Pet here your prodact..." className="search" onChange={myRecipeSearch} value={mySearch}/>
       </form>
       <button className="icon">
-        <img src={arrow} alt="arrow" width="30px" />
+      <img src={arrow} alt="arrow" width="30px" />
       </button>
-    </div>
-
-    <div>
+   </div>
+   <div>
       {showRecipes.map((element, index) => (
-        <Recipies key={index}
-        label={element.recipe.label}
-        image={element.recipe.image}
-        cousin={element.recipe.cuisineType}
-        time={element.recipe.totalTime}
-        ingredients={element.recipe.ingredientLines}
-        />
+      <Recipies key={index}
+         label={element.recipe.label}
+         image={element.recipe.image}
+         cousin={element.recipe.cuisineType}
+         time={element.recipe.totalTime}
+         ingredients={element.recipe.ingredientLines}
+         />
       ))}
-    </div>
-
-
-
-
-    </div>
-  )
+   </div>
+</div>
+)
 }
--- src/Recipies.js
+++ src/Recipies.js
@@ -1,29 +1,23 @@
 export default function Recipies({label, image, cousin, time, ingredients}) {
     return(
-        <div>
-            <div className="container">
-            <h2>{label}</h2>
-            </div>
-
-            <div className="container">
-                <img src={image} alt="dish"/>
-            </div>
-
-            <div className="container">
-                <h4>{cousin} cousin</h4>
-            </div>  
-        
-            <div className="container">
-                <p>{time} min</p>
-            </div> 
-
-            <ul className="list">
-                {ingredients.map(ingredient => (
-                    <li>{ingredient}</li>
-                ))}
-            </ul>
-
-
-        </div>
+    <div>
+       <div className="container">
+          <h2>{label}</h2>
+       </div>
+       <div className="container">
+          <img src={image} alt="dish"/>
+       </div>
+       <div className="container">
+          <h4>{cousin} cousin</h4>
+       </div>
+       <div className="container">
+          <p>{time} min</p>
+       </div>
+       <ul className="list">
+          {ingredients.map(ingredient => (
+          <li>{ingredient}</li>
+          ))}
+       </ul>
+    </div>
     )
-}
+    }
